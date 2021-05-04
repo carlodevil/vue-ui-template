a5e5e582d52f77956d12991dd9c7be0d0466abea
Carlo de Villiers <carlodevil1@gmail.com>
2021-05-04 11:54:58 +0200

    Added demo component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 
 export { default as TestButton } from '../lib/generic/buttons/test-button/src/TestButton.vue';
 export { default as GenericTitle } from '../lib/generic/title/generic-title/src/GenericTitle.vue';
+export { default as TestComponent } from '../lib/test/test-component/src/TestComponent.vue';
